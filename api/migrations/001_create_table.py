fd3a20e55df9bd278e726b355d5fb322cefc6afe
Eli Spicer <spicefe@gmail.com>
2023-06-28 15:32:30 -0400

    Added adventure reference to location DB table

--- api/migrations/001_create_table.py
+++ api/migrations/001_create_table.py
@@ -6,7 +6,8 @@ steps = [
             id SERIAL PRIMARY KEY NOT NULL,
             address VARCHAR(350) NOT NULL,
             latitude DECIMAL NOT NULL,
-            longitude DECIMAL NOT NULL
+            longitude DECIMAL NOT NULL,
+            adventure_id INTEGER NOT NULL REFERENCES adventures(id)
         );
         """,
         # "Down" SQL statement
