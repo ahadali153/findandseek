dd50f959f379550b3d3d29cd15c0c707b239379b
Jasmine Gilson <gilsonjasmine@gmail.here>
2023-06-29 18:04:51 -0400

    Working on Backend Auth

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -1,4 +1,34 @@
-import os
-from psycopg_pool import ConnectionPool
+# import os
+# from psycopg_pool import ConnectionPool
 
-pool = ConnectionPool(conninfo=os.environ.get("DATABASE_URL"))
+# pool = ConnectionPool(conninfo=os.environ.get("DATABASE_URL"))
+
+from pydantic import BaseModel
+
+
+class DuplicateAccountError(ValueError):
+    pass
+
+
+class AccountIn(BaseModel):
+    email: str
+    password: str
+    username: str
+
+
+class AccountOut(BaseModel):
+    id: str
+    email: str
+    username: str
+
+
+class AccountOutWithPassword(AccountOut):
+    hashed_password: str
+
+
+class AccountQueries:
+    def get(self, email: str) -> AccountOutWithPassword:
+        pass
+
+    def create(self, info: AccountIn, hashed_password: str) -> AccountOut:
+        pass
