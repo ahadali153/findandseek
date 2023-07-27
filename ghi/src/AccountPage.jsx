f6d8498b8275f34f64a3d599de8bdbcd03ff25b9
Ahad <ahadali153@gmail.com>
2023-07-27 13:59:03 -0400

    changed table column

--- ghi/src/AccountPage.jsx
+++ ghi/src/AccountPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from "react-router-dom";
+import { Link } from "r peact-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 
 const UserAccountPage = () => {
