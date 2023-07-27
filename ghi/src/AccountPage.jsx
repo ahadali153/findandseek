d33746627a548df5c8ea7a80e1cc3a5d105c2522
Artemis <chunliu89@outlook.com>
2023-07-27 09:48:02 -0400

    updated journal and account

--- ghi/src/AccountPage.jsx
+++ ghi/src/AccountPage.jsx
@@ -2,20 +2,21 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
+import { useImageUploader } from './AccountImageUploader'; 
 
 const UserAccountPage = () => {
-    const [userData, setUserData] = useState({});
+    const [userData, setUserData] = useState(null);
     const [userAdventures, setUserAdventures] = useState([]);
     const [selectedImage, setSelectedImage] = useState(null);
-
+    const { imageUrl, uploadImage } = useImageUploader();
 
     const fetchUserData = async () => {
     try {
-    const response = await axios.get('http://localhost:8000/token', {
-        withCredentials: true,
-    });
-    console.log(response.data.account);
-    setUserData(response.data.account);
+        const response = await axios.get('http://localhost:8000/token', {
+            withCredentials: true,
+        });
+        console.log(response.data.account);
+        setUserData(response.data.account);
     } catch (error) {
     console.error('Error fetching user data:', error);
     }
@@ -27,48 +28,54 @@ const UserAccountPage = () => {
                 withCredentials: true,
             });
             console.log(response.data);
-            console.log(userData)
+            console.log(userData, "cheese")
             // Filter the adventures based on userData.id
             const filteredAdventures = response.data.filter(
                 (item) => item.account_id == userData.id
             );
             console.log("filtered:", filteredAdventures)
             setUserAdventures(filteredAdventures);
-            } catch (error) {
-            console.error('Error fetching adventures:', error);
-            }
-        };
+        } catch (error) {
+        console.error('Error fetching adventures:', error);
+        }
+    };
 
     useEffect(() => {
         // Fetch user data and user adventures concurrently
-        Promise.all([fetchUserData(), fetchUserAdventures()]);
+        Promise.all([fetchUserData()]);
     }, []);
+    
     const handleProfilePictureChange = (event) => {
         const file = event.target.files[0];
         setSelectedImage(file);
     };
 
-    const handleProfilePictureUpload = async () => {
-        console.log(setSelectedImage)
-        if (!selectedImage) {
-            console.error('No image uploaded.');
-            return;
+    useEffect(() => {
+        if (userData) {
+            fetchUserAdventures()
         }
+    }, [userData]);
 
-        const formData = new FormData();
-        formData.append('profilePicture', selectedImage);
+    const handleProfilePictureUpload = async () => {
+		if (!selectedImage) {
+            console.error("No image uploaded.");
+            return;
+	}
+        
+    const formData = new FormData();
+    formData.append('profilePicture', selectedImage);
 
         try {
-            const response = await axios.post('http://localhost:8000/upload-profile-picture', formData, {
+            const response = await axios.post('http://localhost:8000/account/upload-profile-picture', formData, {
                 headers: {
-                    'Content-Type': 'multipart/form-data',
+                'Content-Type': 'multipart/form-data',
                 },
             });
             if (response.status === 200) {
                 const uploadedImageUrl = response.data.imageUrl;
                 setUserData((prevUserData) => ({
-                    ...prevUserData,
-                    profile_picture: uploadedImageUrl,
+                ...prevUserData,
+                profile_picture: uploadedImageUrl,
                 }));
                 console.log('Profile picture uploaded successfully.');
             } else {
@@ -76,37 +83,34 @@ const UserAccountPage = () => {
             }
         } catch (error) {
             console.error('Error uploading profile picture:', error);
-        }
-    };
+            }
+        };
 
     return (
-        <div>
-            <h1>Welcome to {userData?.email}'s Page!</h1>
-            <p>User PFP: {userData.profile_picture}</p>
             <div>
-                {userAdventures.map((adventure) => (
-                    <div key={adventure.id}>
-                        <h3>{adventure.title}</h3>
-                        <p>{adventure.description}</p>
-                        <img src={adventure.image_url} alt={adventure.title} />
+            {userData && (
+                <>
+                    <h1>Welcome to {userData?.email}'s Page!</h1>
+                    <p>User PFP: {userData.profile_picture}</p>
+                    <h6>Upload Profile Picture</h6>
+                    <input 
+                        type="file" 
+                        onChange={handleProfilePictureChange} />
+                    <button onClick={handleProfilePictureUpload}>Upload</button>
+                    {imageUrl && <img src={imageUrl} alt="Upload Profile Picture" />}
+                    <h2>Your Posts:</h2>
+                    <div>
+                        {userAdventures.map((adventure) => (
+                            <div key={adventure.id}>
+                                <h3>{adventure.title}</h3>
+                                <p>{adventure.description}</p>
+                                <img src={adventure.image_url} alt={adventure.title} />
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-            <h6>Upload Profile Picture</h6>
-            <input 
-                type="file" 
-                onChange={handleProfilePictureChange} />
-            <button onClick={handleProfilePictureUpload}>Upload</button>
-            <h2>Your Posts:</h2>
-{/* 
-            <div className="mb-3">
-                <label className="form-label">Image:</label>
-                <input
-                    type="file"
-                    className="form-control"
-                    onChange={handleProfilePictureChange}
-                />
-            </div> */}
+                </>
+
+            )}
         </div>
     );
 };
