c58b89a15105383a11490825515c8bd1b2803483
Eli Spicer <spicefe@gmail.com>
2023-07-27 19:34:16 -0400

    Readme GHI updated; Account Page + Nav Bar CSS Updated

--- ghi/src/AccountPage.jsx
+++ ghi/src/AccountPage.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import UploadBioPic from "./AccountInfoUpload"
+import { Link, useParams } from "react-router-dom";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { getRatingIcon, getPriceIcon, getIntensityIcon } from "./GoogleMap";
 
 const UserAccountPage = () => {
 
     const [userData, setUserData] = useState(null);
     const [userAdventures, setUserAdventures] = useState([]);
+    const [activityMap, setActivityMap] = useState({});
 
     const fetchUserData = async () => {
 
@@ -35,9 +39,36 @@ const UserAccountPage = () => {
             console.log("filtered:", filteredAdventures)
             setUserAdventures(filteredAdventures);
         } catch (error) {
-        console.error('Error fetching adventures:', error);
+            console.error('Error fetching adventures:', error);
         }
     };
+
+    const fetchAllActivities = async () => {
+		try {
+            const response = await fetch("http://localhost:8000/activities");
+            const data = await response.json();
+            return data;
+		} catch (error) {
+            console.log("Error fetching activities:", error);
+            return [];
+		}
+	};
+
+	// Helper function to fetch activity name based on activity ID
+	const getActivityName = (activityId) => {
+		return activityMap[activityId] || "Unknown Activity";
+	};
+
+	useEffect(() => {
+        // Fetch all activities and map them to their IDs
+        fetchAllActivities().then((activities) => {
+        const mappedActivities = activities.reduce((acc, activity) => {
+            acc[activity.id] = activity.name;
+            return acc;
+        }, {});
+        setActivityMap(mappedActivities);
+        });
+    }, []);
 
     useEffect(() => {
         // Fetch user data and user adventures concurrently
@@ -53,26 +84,86 @@ const UserAccountPage = () => {
     }, [userData]);
 
     return (
-            <div>
+            <Container className="py-4">
             {userData && (
                 <>
-                    <h1>Welcome to {userData?.email}'s Page!</h1>
-                    <p>User PFP: {userData.profile_picture}</p>
-                    <h6>Upload Profile Picture</h6>
-                    <UploadBioPic />
-                    <h2>Your Posts:</h2>
-                    <div>
+                    <Row className="mt-4">
+                        <Col md={{ span: 6 }}>
+                        <Card className="card-background">
+                            <Card.Body>
+                            <Card.Title className="text-center">Welcome to {userData?.email}'s Page!</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        </Col>
+                    </Row>
+                    <Row className="mt-4">
+                        <Col md={{ span: 6 }}>
+                        <Card className="card-background">
+                            <Card.Body>
+                            <Card.Title className="text-center">User PFP: {userData.profile_picture}</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        </Col>
+                    </Row>
+                    <Row className="mt-4">
+                        <Col md={{ span: 6 }}>
+                        <Card className="card-background">
+                            <Card.Body>
+                            <Card.Title className="text-center">Upload Profile Picture</Card.Title>
+                                <Card.Text className="text-center">
+                                    <UploadBioPic />
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        </Col>
+                    </Row>
+                    <Row className="mt-4 py-2">
+                        <Col md={{ span: 6 }}>
+                        <Card className="card-background">
+                            <Card.Body>
+                            <Card.Title className="text-center">Your posts:</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        </Col>
+                    </Row>
+                    <Row className="card-container">
                         {userAdventures.map((adventure) => (
-                            <div key={adventure.id}>
-                                <h3>{adventure.title}</h3>
-                                <p>{adventure.description}</p>
-                                <img src={adventure.image_url} alt={adventure.title} />
-                            </div>
+                            <Col key={adventure.id} md={3} style={{ marginBottom: "20px" }}>
+                            <Link to={`/adventures/${adventure.id}`} style={{ textDecoration: "none" }}>
+                                <div className="card" style={{ width: "18rem" }}>
+                                <img
+                                    src={adventure.image_url}
+                                    className="card-img-top"
+                                    alt="Adventure Thumbnail"
+                                    style={{
+                                    objectFit: "cover",
+                                    height: "150px",
+                                    borderTopLeftRadius: "12px",
+                                    borderTopRightRadius: "12px",
+                                    }}
+                                />
+                                <div className="card-body">
+                                    <h5 className="card-title card-center">{adventure.title}</h5>
+                                    <p className="card-text card-center">{getActivityName(adventure.activity_id)}</p>
+                                    <p className="card-text card-center">{adventure.address}</p>
+                                    <p className="card-text">
+                                        User Rating: <img className="icon" src={getRatingIcon(adventure.user_rating)} alt="Rating" />
+                                    </p>
+                                    <p className="card-text">
+                                        Price: <img className="icon" src={getPriceIcon(adventure.price)} alt="Price" />
+                                    </p>
+                                    <p className="card-text">
+                                        Intensity: <img className="icon" src={getIntensityIcon(adventure.intensity)} alt="Intensity" />
+                                    </p>
+                                </div>
+                                </div>
+                            </Link>
+                            </Col>
                         ))}
-                    </div>
+                    </Row>
                 </>
             )}
-        </div>
+        </Container>
     );
 };
 
